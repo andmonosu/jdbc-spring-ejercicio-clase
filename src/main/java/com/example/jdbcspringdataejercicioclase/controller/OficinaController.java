0fb0c7e8a6ecc9cc6d578bc2186bed651970d83f
Andreu Montagut Osuna <andreumontagut@gmail.com>
2025-05-07 13:40:15 +0200

    Added workes into office view

--- src/main/java/com/example/jdbcspringdataejercicioclase/controller/OficinaController.java
+++ src/main/java/com/example/jdbcspringdataejercicioclase/controller/OficinaController.java
@@ -1,5 +1,6 @@
 package com.example.jdbcspringdataejercicioclase.controller;
 
+import com.example.jdbcspringdataejercicioclase.model.Oficina;
 import com.example.jdbcspringdataejercicioclase.service.EmpleadoService;
 import com.example.jdbcspringdataejercicioclase.service.OficinaService;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class OficinaController {
 
     private final OficinaService oficinaService;
+    private final EmpleadoService empleadoService;
 
-    public OficinaController(OficinaService oficinaService) {
+    public OficinaController(OficinaService oficinaService, EmpleadoService empleadoService) {
         this.oficinaService = oficinaService;
+        this.empleadoService = empleadoService;
     }
 
     @GetMapping("/oficinas")
@@ -25,8 +28,9 @@ public class OficinaController {
 
     @GetMapping("/oficinas/{id}")
     public String showOficina(Model model, @PathVariable String id) {
-        model.addAttribute("oficina", oficinaService.findById(id));
-        model.addAttribute("titulo", "Lista de Oficinas");
+        Oficina oficina = oficinaService.findById(id).orElse(new Oficina());
+        model.addAttribute("oficina", oficina);
+        model.addAttribute("empleados", empleadoService.findByCodigoOficina(oficina.getCodigoOficina()));
         return "editarOficina";
     }
 }
